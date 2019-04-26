c100006bf67836a1ddc3b731b97330f4ced74f13
pollonet <30765345+pollonet@users.noreply.github.com>
2019-04-27 04:09:58 +0700

    Add files via upload

---
+++ supersoccerlive1.js
@@ -0,0 +1,3 @@
+    var go_url = 'https://suarankri.me/';
+    var api = '8dfe678b2c7b300c005e01bc2b5c98a008dd13bc';
+    var shorten_exclude = ['google.com', 'www.supersoccerlive.id', 'supersoccerlive.id']; 
